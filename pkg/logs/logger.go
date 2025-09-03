d0525b2aa8f777d20f88eec6201e2cb096e7716c
Stanislav Kuptsov <st.kuptsov@gmail.com>
2025-09-03 17:45:01 +0300

    fix gitignore

---
+++ pkg/logs/logger.go
@@ -0,0 +1,83 @@
+package log
+
+import (
+	"os"
+
+	"github.com/st-kuptsov/mail2tg/config"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+func logLevel(level string) zapcore.Level {
+	switch level {
+	case "debug":
+		return zapcore.DebugLevel
+	case "info":
+		return zapcore.InfoLevel
+	case "warning":
+		return zapcore.WarnLevel
+	case "error":
+		return zapcore.ErrorLevel
+	default:
+		return zapcore.InfoLevel // По умолчанию INFO
+	}
+}
+
+func DefaultLogger(logConfig config.LogConfig) *zap.SugaredLogger {
+	logPath := logConfig.Directory + "/" + logConfig.Filename
+
+	// Конфигурация файла
+	fileWriter := zapcore.AddSync(&lumberjack.Logger{
+		Filename:   logPath,
+		MaxSize:    logConfig.MaxSize,
+		MaxBackups: logConfig.MaxBackups,
+		MaxAge:     logConfig.MaxAge,
+		Compress:   logConfig.Compress,
+	})
+
+	// Уровень логирования
+	level := logLevel(logConfig.Level)
+
+	// Конфигурация кодировщиков
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:       "time",
+		LevelKey:      "level",
+		NameKey:       "logger",
+		CallerKey:     "caller",
+		MessageKey:    "msg",
+		StacktraceKey: "stacktrace",
+		EncodeTime:    zapcore.ISO8601TimeEncoder,
+		EncodeLevel:   zapcore.CapitalLevelEncoder,
+		EncodeCaller:  zapcore.ShortCallerEncoder,
+	}
+
+	// JSON логирование в файл
+	jsonEncoder := zapcore.NewJSONEncoder(encoderConfig)
+	fileCore := zapcore.NewCore(
+		jsonEncoder,
+		fileWriter,
+		level,
+	)
+
+	var core zapcore.Core
+
+	// JSON логирование в консоль
+	if logConfig.Console {
+
+		consoleEncoder := zapcore.NewJSONEncoder(encoderConfig)
+		consoleCore := zapcore.NewCore(
+			consoleEncoder,
+			zapcore.AddSync(os.Stdout),
+			level,
+		)
+		core = zapcore.NewTee(fileCore, consoleCore)
+	} else {
+		core = fileCore
+	}
+
+	// Создание логгера
+	logger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
+	return logger.Sugar()
+}
